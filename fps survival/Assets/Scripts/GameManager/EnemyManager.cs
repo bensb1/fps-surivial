60f6009cc52e0a890fdc2cac51aa098ffd763e83
bensb1 <56164275+bensb1@users.noreply.github.com>
2020-11-09 14:24:27 +0000

    before removing enemy from hierarchy

---
+++ fps survival/Assets/Scripts/GameManager/EnemyManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager instance;
+    [SerializeField]
+    private GameObject cannibal_Prefab;
+    public Transform[] cannibal_SpawnPoints;
+    [SerializeField]
+    private int cannibal_Enemy_Count;
+    private int initial_Cannibal_Count;
+    public float wait_Before_Spawn_Enemies_Time = 10f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    void MakeInstance()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+}
+
